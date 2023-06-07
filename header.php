174ad8f2cf744df0477695b3325cd6b46dfcbad1
guiguivida183882 <guikerme.1707@gmail.com>
2023-06-07 11:19:44 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<header>
+    <div class="logo-header">AGENCIA GSA</div>
+    <div class="hamburguer">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+    <nav class="nav-bar">
+        <ul>
+            <li>
+                <a href="" class="active">Home</a>
+            </li>
+            <li>
+                <a href="">Featrure</a>
+            </li>
+            <li>
+                <a href="">Pricinf</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <script src="js/script.js"></script>
+
+</header>
