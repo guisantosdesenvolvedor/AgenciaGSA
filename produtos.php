fc606574989f698e494a84e36c9f2e0d944de3a4
guiguivida183882 <guikerme.1707@gmail.com>
2023-06-11 22:09:33 -0300

    fazendo modal em produtos

--- produtos.php
+++ produtos.php
@@ -4,16 +4,32 @@
     </div>
     <div class="modals-produtos">
         <div class="modal-produto">
-            
+            <div class="conteudo-modal">
+                <a href=""><h2>Gestões de Redes socias</h2></a>
+                <a href=""><img src="img/service_img1-active-1.webp" alt=""></a> 
+                <h3>ANÁLISE COMPLETA, BENCHMARKETING E PLANO ESTRUTURAL</h3>
+            </div>
         </div>
         <div class="modal-produto">
-
+            <div class="conteudo-modal">
+                <a href=""><h2>Designer e criação publicitária</h2></a>
+                <a href=""><img src="img/service_img2-active-1.webp" alt=""></a> 
+                <h3>CRIAÇÃO DE IDENTIDADES VISUAIS E SUAS DEMANDAS</h3>
+            </div>
         </div>
         <div class="modal-produto">
-
+            <div class="conteudo-modal">
+                <a href=""><h2>Gestões de Redes socias</h2></a>
+                <a href=""><img src="img/service_img3-active-1.webp" alt=""></a> 
+                <h3>DESENVOLVIMENTO DE WEBSITES E PRODUÇÃO DE CONTEÚDO</h3>
+            </div>
         </div>
         <div class="modal-produto">
-
+            <div class="conteudo-modal">
+                <a href=""><h2>Gestões de Redes socias</h2></a>
+                <a href=""><img src="img/service_img4-active-1.webp" alt=""></a> 
+                <h3>ESTRATÉGICA FOCADA EM MARKETING E SEO</h3>
+            </div>
         </div>
     </div>
 </div>
